c7e8efb9350a570a95541305c09cd411b4856ae9
Matt Pestridge <mpestridge1986@gmail.com>
2016-11-11 15:08:11 -0700

    my code passes all of the test requirement for read and create

--- pets.js
+++ pets.js
@@ -2,24 +2,59 @@
 
 var fs = require('fs');
 var path = require('path');
-var guestsPath = path.join(__dirname, 'guests.json');
+var petsPath = path.join(__dirname, 'pets.json');
 
 var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
 if (cmd === 'read') {
-fs.readFile(guestsPath, 'utf8', function(err, data) {
+fs.readFile(petsPath, 'utf8', function(err, data) {
+  var pets = JSON.parse(data);
+  var index = process.argv[3];
   if (err) {
     throw err;
+  } if (index === undefined) {
+    console.log(pets);
+  } else if (index !== undefined) {
+    if (pets[index] === undefined) {
+      console.error(`Usage: ${node} ${file} read INDEX`);
+      process.exit(1);
+    } else {
+      console.log(pets[index]);
+    }
   }
-
-  var guests = JSON.parse(data);
-
-  console.log(guests);
+  });
+}
+else if (cmd === 'create') {
+fs.readFile(petsPath, 'utf8', function(readErr, data) {
+  var pets = JSON.parse(data);
+  var ageValue = parseInt(process.argv[3]);
+  var kindValue = process.argv[4];
+  var nameValue = process.argv[5];
+  var newPet = {age: ageValue, kind: kindValue, name: nameValue};
+  if (readErr) {
+    throw readErr;
+  } else if (!ageValue || !kindValue || !nameValue) {
+    console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
+    process.exit(1);
+  } else if (ageValue === isNaN) {
+    console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
+    process.exit(1);
+  } else {
+    pets.push(newPet);
+  }
+    var petsJSON = JSON.stringify(pets);
+    fs.writeFile(petsPath, petsJSON, function(writeErr) {
+      if (writeErr) {
+        throw writeErr;
+      } else {
+        console.log(newPet);
+      }
+    });
 });
 }
 else {
-  console.error(`Usage: ${node} ${file} read`);
+  console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
   process.exit(1);
 }
